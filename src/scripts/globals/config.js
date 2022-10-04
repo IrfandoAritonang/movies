e756eed6063b4c3a7418416d8ae63fce9411ff03
Irfando <irfandoaritonang86@gmail.com>
2022-10-04 12:05:37 +0700

    resolved bug

---
+++ src/scripts/globals/config.js
@@ -0,0 +1,8 @@
+const CONFIG = {
+  KEY: 'ca51bd9f7fd24ba9d08ccb933006fb2a',
+  BASE_URL: 'https://api.themoviedb.org/3/',
+  BASE_IMAGE_URL: 'https://image.tmdb.org/t/p/w500/',
+  DEFAULT_LANGUAGE: 'en-us',
+};
+
+export default CONFIG;
